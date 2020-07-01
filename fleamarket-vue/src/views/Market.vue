4001b9b6c4c4e9f5154fd1327d7cf7bcc37dce8d
wenjiaxiao <32972768+wenjiaxiao@users.noreply.github.com>
2020-07-01 11:38:48 +0800

    小bug

--- fleamarket-vue/src/views/Market.vue
+++ fleamarket-vue/src/views/Market.vue
@@ -128,7 +128,7 @@
         methods: {
             onFilterByClass(index) {
                 this.classIndex = index;
-                this.getCommodityList(0);
+                console.log(this.classIndex + 1)
             },
             onSearch() {
                 this.getCommodityList(0);
@@ -148,7 +148,7 @@
                 const _this = this;
                 let url = '';
                 let form = new FormData();
-                if (this.classIndex) {
+                if (this.classIndex || this.classIndex === 0) {
                     form.append('commodity_type', this.classIndex + 1);
                 }
                 if (this.input !== '') {
